0f524357a4722010a73ccdd55e58cdaa37c997f6
henri <henrievert32@gmail.com>
2024-08-05 15:44:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function applyAspectRatioStyles() {
 applyAspectRatioStyles();
 
 window.addEventListener('resize', applyAspectRatioStyles);
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
